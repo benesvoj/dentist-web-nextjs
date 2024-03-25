da2a2c9be14ad250fbf8469941fa9e30c6c39eb2
Vojtech Benes <vojtech.benes@carvago.com>
2024-03-25 11:57:35 +0100

    add hiding navlinks in admin if the module is not available

--- src/app/(admin)/admin/dashboard/ui/NavLinks.tsx
+++ src/app/(admin)/admin/dashboard/ui/NavLinks.tsx
@@ -14,7 +14,11 @@ export default function NavLinks() {
     <>
       {links.map((link) => {
         const LinkIcon = link.icon
-        const isDisabled = !contentSetupData.find((data) => data?.name === link.contentPage && !data?.isVisible)
+        const isDisabled = !!contentSetupData.find((data) => data?.name === link.contentPage && !data?.isVisible)
+
+        if (isDisabled) {
+          return null
+        }
 
         return (
           <Link
@@ -22,7 +26,6 @@ export default function NavLinks() {
             href={link.href}
             className={clsx('flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
               {'bg-sky-100 text-blue-600': pathname === link.href})}
-            aria-disabled={isDisabled}
           >
             <LinkIcon className="w-6" />
             <p className="hidden md:block">{link.name}</p>
